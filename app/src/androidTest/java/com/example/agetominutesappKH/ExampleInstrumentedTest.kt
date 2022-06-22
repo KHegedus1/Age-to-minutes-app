42d17a166d20da18171fc08f0c9631cc93560d42
KHegedus1 <kico.kiki@gmail.com>
2022-06-22 21:48:53 +0200

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/agetominutesappKH/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/agetominutesappKH/ExampleInstrumentedTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
-import org.junit.AfterClass
+import org.junit.AfterClass 
 
 /**
  * Instrumented test, which will execute on an Android device.

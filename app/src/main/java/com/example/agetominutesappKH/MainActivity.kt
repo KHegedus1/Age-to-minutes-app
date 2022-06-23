17b149c491a26b60dd4d1d3f92d2aeb7a4b69b10
KHegedus1 <kico.kiki@gmail.com>
2022-06-23 22:11:08 +0200

    current date + minutes

--- app/src/main/java/com/example/agetominutesappKH/MainActivity.kt
+++ app/src/main/java/com/example/agetominutesappKH/MainActivity.kt
@@ -43,6 +43,14 @@ class MainActivity : AppCompatActivity() {
         val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
 
         val theDate = sdf.parse(selectedDate)
+
+        val selectedDateInMinutes = theDate.time/60000
+
+        val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+        val currentDateInMinutes = currentDate.time/60000
+
+        val differenceInMinutes = currentDate - selectedDateInMinutes
     },
     year,
     month,
